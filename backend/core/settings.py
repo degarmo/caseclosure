d0be89cdac44b9bcf6a5f6351a9b8e7347e4bc4f
Cory <degarmo@gmail.com>
2025-07-06 22:01:04 -0500

    feat: add Django admin, PostgreSQL config, and login/signup flow

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caseclosure_db',         # your db name
+        'USER': 'cdgsql',           # your postgres user
+        'PASSWORD': 'Zuzu2247!!',      # your postgres password
+        'HOST': 'localhost',              # or your DB host
+        'PORT': '5432',                   # default postgres port
     }
 }
+
 
 
 # Password validation
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import './App.css';
+import './assets/styles/tailwind.css';
+import './assets/styles/index.css'
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         {/* Add your other routes here, e.g. Login, Dashboard */}
       </Routes>
     </Router>
