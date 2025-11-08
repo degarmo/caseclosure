a5e007ea0b0320841e2e6fe2597c5afd8ae14c73
Cory <degarmo@gmail.com>
2025-11-07 20:29:39 -0600

    updating db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'contact',
     'caseboard',
+    'django_filters',
 ]
 
 # Registration Settings
@@ -263,8 +264,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
     ],
     'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny', 
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': [
